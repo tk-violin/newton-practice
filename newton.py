63be666d47ac7e679c5af112c4028200601acb7b
tk-violin <taiki_kubo@berkeley.edu>
2024-08-22 13:36:56 -0700

    Add explanation to newton.py file.

--- newton.py
+++ newton.py
@@ -1,18 +1,33 @@
 def f(x):
-    return x**2 + 2 * x + 1
+    return x**2 + 2 * x + 1  #Just example. You can
 
 
 def deriv(func, x):
+    """
+    This function takes first-order derivative.
+    e = scalar (significantly small number)
+    """
     e = pow(10, -6)
     return (func(x + e) - func(x)) / e
 
 
 def sec_deriv(func, x):
-    e = pow(10, -6)
+    """
+    This function takes second-order derivative.
+    e = scalar (significantly small number)
+    """
+    e = pow(10, -6)   # significantly 
     return (deriv(func, x + e) - deriv(func, x)) / e
 
 
 def newton(func):
+    """
+    This function estimates x to minimize f(x) defined above using Newton method.
+    new = new x candidate (0 is the primer.)
+    preb = prebious x candidate (100 is the primer.)
+    p = scalar (significantly small number)
+    once the difference between new and preb reaches smaller than p, the cycle ends. 
+    """
     new = 0
     preb = 100
     p = pow(10, -6)
